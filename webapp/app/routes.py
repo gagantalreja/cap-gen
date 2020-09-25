f73a4495f8203a05a171cdf7009bfd84bf608463
gagantalreja <gtcms1@gmail.com>
2020-09-25 18:12:10 +0530

    push

---
+++ webapp/app/routes.py
@@ -0,0 +1,35 @@
+from flask import request, jsonify, Blueprint, render_template
+from flask import url_for, redirect
+import requests, numpy
+import app.model as model
+
+main_bp = Blueprint('main_bp', __name__)
+
+@main_bp.route('/', methods=["GET"])
+def index():
+    return render_template('index.html')
+
+@main_bp.route('/upload', methods=["POST"])
+def upload():
+    data = request.files.get("file")
+    x = data.content_type.split("/")[1]
+    data.save(f"./app/static/image.{x}")
+    return jsonify(src=url_for("static", filename=f"image.{x}"))
+
+@main_bp.route('/get-result', methods=["GET"])
+def get_res():
+    x = request.args.get("image")
+    x = f"./app/{x}"
+    x = model.main(x).lower()
+    #print(request.base_url, request.referrer)
+    output = requests.get(f"{request.referrer}text-api?q={x}").json()
+    return jsonify(result=output)
+
+@main_bp.route("/text-api", methods=["GET"])
+def text_api():
+    emotion = request.args.get("q")
+    if emotion == "happy":
+        r = requests.get("http://localhost:3000/")
+        return jsonify(r.json())
+    else:
+        return jsonify(result="")
